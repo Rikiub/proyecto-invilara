eb007184d9df805665754cc6dcfc4700f35062e3
Rikiub <rikiub.dev@gmail.com>
2024-06-11 18:25:06 -0400

    Cambios

--- index.php
+++ index.php
@@ -2,14 +2,3 @@
 
 require_once "src/rutas.php";
 $router->despachar();
-
-
-/**
-if (isset($_GET['pagina'])) {
-    $pagina = $_GET['pagina'];
-} else {
-    $pagina = 'sesion';
-}
-
-require "src/controlador/" . $pagina . ".php";
- */
