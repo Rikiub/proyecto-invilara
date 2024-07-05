0a150f2ddc49f07f4ea8a6e7fef554fcda225240
Rikiub% <rikiub.dev@gmail.com>
2024-07-05 01:21:08 -0400

    Update

--- invilara.sql
+++ invilara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 26, 2024 at 07:19 PM
+-- Generation Time: Jul 05, 2024 at 04:40 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -24,33 +24,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registro_1x10`
+-- Table structure for table `comunidad`
 --
 
-CREATE TABLE `registro_1x10` (
-  `id_alcaldia` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `parroquia` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `municipio` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `descripcion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `estatus` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `solicitante` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  `telefono` int DEFAULT NULL,
-  `cedula` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `comunidad` (
+  `id` int NOT NULL,
+  `id_parroquia` int DEFAULT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `direccion` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `tipo` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registro_solicitud`
+-- Table structure for table `director`
 --
 
-CREATE TABLE `registro_solicitud` (
+CREATE TABLE `director` (
   `id` int NOT NULL,
-  `nombre_comunidad` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `direccion_comunidad` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `fecha` date NOT NULL,
-  `problematica` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  `direccion` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `correo` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gerencia`
+--
+
+CREATE TABLE `gerencia` (
+  `id` int NOT NULL,
+  `id_director` int NOT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `gerente` varchar(45) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `institucion`
+--
+
+CREATE TABLE `institucion` (
+  `id` int NOT NULL,
+  `id_director` int NOT NULL,
+  `id_parroquia` int NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `correo` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `telefono` int NOT NULL,
+  `direccion` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `dependencia` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `municipio`
+--
+
+CREATE TABLE `municipio` (
+  `id` int NOT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parroquia`
+--
+
+CREATE TABLE `parroquia` (
+  `id` int NOT NULL,
+  `id_municipio` int NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -60,18 +111,41 @@ CREATE TABLE `registro_solicitud` (
 
 CREATE TABLE `solicitante` (
   `cedula` int NOT NULL,
-  `nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `correo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `telefono` int NOT NULL,
-  `direccion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_comunidad` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `correo` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `telefono` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `solicitante`
+-- Table structure for table `solicitud`
 --
 
-INSERT INTO `solicitante` (`cedula`, `nombre`, `correo`, `telefono`, `direccion`) VALUES
-(0, '', '', 0, '');
+CREATE TABLE `solicitud` (
+  `nro_solicitud` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `cedula_solicitante` int DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `nombre_comunidad` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `problematica` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `estatus` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `solicitud_1x10`
+--
+
+CREATE TABLE `solicitud_1x10` (
+  `nro_control` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `cedula_solicitante` varchar(100) DEFAULT NULL,
+  `id_parroquia` int DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `estatus` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -80,17 +154,17 @@ INSERT INTO `solicitante` (`cedula`, `nombre`, `correo`, `telefono`, `direccion`
 --
 
 CREATE TABLE `solicitud_institucional` (
-  `nro_control` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nro_oficio` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `fecha` datetime NOT NULL,
-  `asunto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `telefono` int NOT NULL,
-  `observacion` int NOT NULL,
-  `gerencia_referida` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `direccion_comunidad` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `instrucciones_presidencial` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `estatus` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nro_control` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `nro_oficio` varchar(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `id_institucion` int DEFAULT NULL,
+  `id_gerencia` int DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  `observacion` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `instrucciones` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `estatus` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `asunto` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -99,55 +173,52 @@ CREATE TABLE `solicitud_institucional` (
 --
 
 CREATE TABLE `solicitud_institucional_director` (
-  `nro_control` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nro_oficio` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `organismo_remitente` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nombre_presidente` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `direccion` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `problematica` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `gerencia_referida` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `estatus` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuario`
---
-
-CREATE TABLE `usuario` (
-  `cedula` int NOT NULL,
-  `contraseña` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+  `nro_control` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `nro_oficio` varchar(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `id_gerencia` int DEFAULT NULL,
+  `problematica` text,
+  `estatus` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Dumping data for table `usuario`
---
-
-INSERT INTO `usuario` (`cedula`, `contraseña`) VALUES
-(1, '1'),
-(11111, '11111'),
-(123, '123'),
-(55, '55'),
-(1234, '1234'),
-(15, '15'),
-(12, '12');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `registro_1x10`
+-- Indexes for table `comunidad`
 --
-ALTER TABLE `registro_1x10`
-  ADD PRIMARY KEY (`id_alcaldia`) USING BTREE;
+ALTER TABLE `comunidad`
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
--- Indexes for table `registro_solicitud`
+-- Indexes for table `director`
 --
-ALTER TABLE `registro_solicitud`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `director`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `gerencia`
+--
+ALTER TABLE `gerencia`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `institucion`
+--
+ALTER TABLE `institucion`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `parroquia`
+--
+ALTER TABLE `parroquia`
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `solicitante`
@@ -156,26 +227,80 @@ ALTER TABLE `solicitante`
   ADD PRIMARY KEY (`cedula`);
 
 --
+-- Indexes for table `solicitud`
+--
+ALTER TABLE `solicitud`
+  ADD PRIMARY KEY (`nro_solicitud`) USING BTREE;
+
+--
+-- Indexes for table `solicitud_1x10`
+--
+ALTER TABLE `solicitud_1x10`
+  ADD PRIMARY KEY (`nro_control`) USING BTREE;
+
+--
 -- Indexes for table `solicitud_institucional`
 --
 ALTER TABLE `solicitud_institucional`
-  ADD PRIMARY KEY (`nro_control`);
+  ADD PRIMARY KEY (`nro_control`) USING BTREE;
 
 --
 -- Indexes for table `solicitud_institucional_director`
 --
 ALTER TABLE `solicitud_institucional_director`
-  ADD PRIMARY KEY (`nro_control`);
+  ADD PRIMARY KEY (`nro_control`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `registro_solicitud`
+-- AUTO_INCREMENT for table `comunidad`
 --
-ALTER TABLE `registro_solicitud`
+ALTER TABLE `comunidad`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `director`
+--
+ALTER TABLE `director`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gerencia`
+--
+ALTER TABLE `gerencia`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `institucion`
+--
+ALTER TABLE `institucion`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `municipio`
+--
+ALTER TABLE `municipio`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parroquia`
+--
+ALTER TABLE `parroquia`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `solicitud`
+--
+ALTER TABLE `solicitud`
+  ADD CONSTRAINT `FK_solicitud_solicitud_1x10` FOREIGN KEY (`nro_solicitud`) REFERENCES `solicitud_1x10` (`nro_control`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_solicitud_solicitud_institucional` FOREIGN KEY (`nro_solicitud`) REFERENCES `solicitud_institucional` (`nro_control`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_solicitud_solicitud_institucional_director` FOREIGN KEY (`nro_solicitud`) REFERENCES `solicitud_institucional_director` (`nro_control`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
